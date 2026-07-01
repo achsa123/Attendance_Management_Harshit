e259335ce87d9fd3418ce24557fe5df03bdbfe8a
Harshit V <harshitvinod084@gmail.com>
2026-07-01 15:38:03 +0530

    Completed attendance feature

--- server/app.js
+++ server/app.js
@@ -182,138 +182,95 @@ message:"Student Updated"
 
 
 // DELETE STUDENT
-
-
 app.delete("/students/:id",(req,res)=>{
 
+    let id=req.params.id;
 
-let id=req.params.id;
+    db.run(
 
+        "DELETE FROM students WHERE id=?",
 
+        [id],
 
-db.run(
+        function(err){
 
-"DELETE FROM students WHERE id=?",
+            if(err)
+                return res.json({error:err.message});
 
-[id],
+            res.json({
 
+                message:"Student Deleted"
 
-function(err){
+            });
 
+        });
 
-if(err)
-return res.json({error:err.message});
-
-
-res.json({
-
-message:"Student Deleted"
-
-});
-
-
-});
-
-
-});
-
-
-
-
-
-
-
+});  
 
 
 // MARK ATTENDANCE
 
+app.post("/attendance", (req, res) => {
 
-app.post("/attendance",(req,res)=>{
+    console.log("BODY RECEIVED:", req.body);
 
+    const {
+        student_id,
+        date,
+        status
+    } = req.body;
 
-const {
+    db.run(
+        `
+        INSERT INTO attendance
+        (student_id, date, status)
+        VALUES (?, ?, ?)
+        `,
+        [student_id, date, status],
 
-student_id,
+        function (err) {
 
-date,
+            if (err) {
+                console.log("SQL ERROR:", err.message);
+                return res.json({
+                    error: err.message
+                });
+            }
 
-status
+            console.log("Attendance inserted!");
 
-}=req.body;
+            res.json({
+                message: "Attendance Marked"
+            });
 
-
-
-db.run(
-
-`
-
-INSERT INTO attendance
-
-(student_id,date,status)
-
-VALUES(?,?,?)
-
-`,
-
-[student_id,date,status],
-
-
-function(err){
-
-
-if(err)
-return res.json({error:err.message});
-
-
-
-res.json({
-
-message:"Attendance Marked"
+        }
+    );
 
 });
-
-
-});
-
-
-});
-
-
-
-
-
-
-
 
 
 // VIEW ATTENDANCE
 
-
 app.get("/attendance",(req,res)=>{
 
+    db.all(
 
-db.all(
+        `
+        SELECT * FROM attendance
+        `,
 
-`
+        [],
 
-SELECT * FROM attendance
+        (err,rows)=>{
 
-`,
+            if(err)
+                return res.json({error:err.message});
 
-[],
+            res.json(rows);
 
-(err,rows)=>{
+        }
 
-
-if(err)
-return res.json({error:err.message});
-
-
-res.json(rows);
-
-
-});
-
+    );
 
 });
 
@@ -324,10 +281,3 @@ res.json(rows);
 
 
 
-app.listen(5000,()=>{
-
-
-console.log("Server running on port 5000");
-
-
-});
